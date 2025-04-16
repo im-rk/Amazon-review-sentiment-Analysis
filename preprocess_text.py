b229b317c92803e26f9e4f34bdcbe6bc7d05bc9c
Ramkumar K R <123564651+im-rk@users.noreply.github.com>
2025-04-16 18:42:12 +0530

    Add files via upload

--- preprocess_text.py
+++ preprocess_text.py
@@ -3,19 +3,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-#from nltk.stem.porter import PorterStemme
 
-# Download required NLTK resources (Run this ONCE before calling preprocess_text)
 nltk.download('stopwords')
 def preprocess_text(text):
-    """
-    Preprocesses the given text by:
-    - Converting to lowercase
-    - Removing URLs, special characters, numbers, and extra spaces
-    - Removing stopwords
-    - Tokenizing
-    - Lemmatizing words
-    """
     text = str(text).lower()  # Convert to lowercase
     text = re.sub(r'https?://\S+|www\.\S+', '', text)  # Remove URLs
     text = re.sub(r'\[.*?\]', '', text)  # Remove text inside square brackets
@@ -26,11 +16,9 @@ def preprocess_text(text):
     # Remove stopwords
     stop_words = set(stopwords.words("english"))
     words = word_tokenize(text)  # Tokenize text
-    #Stemming
+    
     filtered_words = [word for word in words if word not in stop_words]
-    #ps=PorterStemmer()
-    #Stemmed_words = [ps.stem(word) for word in filtered_words]
-    # Lemmatization
+    
     lemmatizer = WordNetLemmatizer()
     lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
 
